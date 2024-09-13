7dded495a13a1931fd8ba54849b8cf611692d783
Electromayonaise <agazazaca@gmail.com>
2024-09-13 17:07:48 -0500

    upload base

---
+++ movies.backend/src/test/java/icesi/movies/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package icesi.movies.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
